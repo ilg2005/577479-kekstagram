0d911924e8c44cc7b09a15561388558382422de4
ilg2005 <igoldfarb10@gmail.com>
2018-12-11 11:37:24 +0300

    модуль upload-image.js работоспособен

--- js/upload-image.js
+++ js/upload-image.js
@@ -1,5 +1,37 @@
 'use strict';
 
 (function () {
+  var ESC_KEYCODE = 27;
 
+  var uploadFileElement = document.querySelector('#upload-file');
+  var pictureEditingElement = document.querySelector('.img-upload__overlay');
+  var cancelEditingElement = pictureEditingElement.querySelector('#upload-cancel');
+  var effectLevelElement = pictureEditingElement.querySelector('.effect-level');
+  var hashtagsElement = pictureEditingElement.querySelector('.text__hashtags');
+  var commentsElement = pictureEditingElement.querySelector('.text__description');
+
+  var documentKeydownEscHandler = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE && evt.target !== hashtagsElement && evt.target !== commentsElement) {
+      cancelImageEditing();
+    }
+  };
+
+  var cancelImageEditing = function () {
+    window.utilities.hideElement(pictureEditingElement);
+    document.removeEventListener('keydown', documentKeydownEscHandler);
+    uploadFileElement.value = '';
+  };
+
+  var cancelEditingElementClickHandler = function () {
+    cancelImageEditing();
+  };
+
+  var uploadFileElementChangeHandler = function () {
+    window.utilities.showElement(pictureEditingElement);
+    window.utilities.hideElement(effectLevelElement);
+    cancelEditingElement.addEventListener('click', cancelEditingElementClickHandler);
+    document.addEventListener('keydown', documentKeydownEscHandler);
+  };
+
+  uploadFileElement.addEventListener('change', uploadFileElementChangeHandler);
 })();
