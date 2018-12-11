66a0cc6d434c268777db6b4e2d38c91a3dbe6006
ilg2005 <igoldfarb10@gmail.com>
2018-12-11 09:49:32 +0300

    модуль gallery.js работоспособен

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,87 @@
 'use strict';
 
 (function () {
+  var pictureNumbers;
+  var pictureTemplateElement = document.querySelector('#picture').content;
+  var picturesElement = document.querySelector('.pictures');
 
+
+  var createArrayFromRange = function (min, max) {
+    var numbers = [];
+    for (var i = min; i <= max; i++) {
+      numbers.push(i);
+    }
+    return numbers;
+  };
+
+  var getRandomIndex = function (array) {
+    return Math.round(Math.random() * (array.length - 1));
+  };
+
+  var getRandomValue = function (array) {
+    return array[getRandomIndex(array)];
+  };
+
+  var getRandomInRange = function (min, max) {
+    var array = createArrayFromRange(min, max);
+    return getRandomValue(array);
+  };
+
+  var getUniqueRandomValue = function (array) {
+    var uniqueRandomValue = getRandomValue(array);
+    array.splice(array.indexOf(uniqueRandomValue), 1);
+    return uniqueRandomValue;
+  };
+
+  var generateCommentsForPicture = function (commentsArray) {
+    var commentsClone = commentsArray.slice();
+    var commentsNumber = getRandomIndex(commentsClone);
+    var commentsForPicture = [];
+    if (commentsNumber !== 0) {
+      for (var i = 0; i <= commentsNumber; i++) {
+        commentsForPicture.push(getUniqueRandomValue(commentsClone));
+      }
+    }
+    return commentsForPicture;
+  };
+
+  var createPicture = function () {
+    var picture = {
+      url: 'photos/' + getUniqueRandomValue(pictureNumbers) + '.jpg',
+      likes: getRandomInRange(window.backend.MIN_LIKES_NUM, window.backend.MAX_LIKES_NUM),
+      comments: generateCommentsForPicture(window.backend.COMMENTS),
+      description: getRandomValue(window.backend.SENTENCES)
+    };
+    return picture;
+  };
+
+  var createPicturesArray = function (picturesNum) {
+    var pictures = [];
+    for (var i = 0; i < picturesNum; i++) {
+      pictures.push(createPicture());
+    }
+    return pictures;
+  };
+
+  var renderPicture = function (picture) {
+    var pictureElement = pictureTemplateElement.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = picture.url;
+    pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+    pictureElement.querySelector('.picture__stat--comments').textContent = picture.comments.length;
+
+    return pictureElement;
+  };
+
+  var renderSimilarPictures = function (pictures) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < pictures.length; i++) {
+      fragment.appendChild(renderPicture(pictures[i]));
+    }
+    picturesElement.appendChild(fragment);
+  };
+
+  pictureNumbers = createArrayFromRange(window.backend.MIN_PICTURE_NUM, window.backend.MAX_PICTURE_NUM);
+  var similarPictures = createPicturesArray(window.backend.MAX_PICTURE_NUM);
+  renderSimilarPictures(similarPictures);
 })();
