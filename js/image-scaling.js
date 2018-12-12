e661118bb15045b32ffe398c75248e2732e65ed7
ilg2005 <igoldfarb10@gmail.com>
2018-12-12 12:03:11 +0300

    исключен лишний парсинг строки при масштабировании

--- js/image-scaling.js
+++ js/image-scaling.js
@@ -1,9 +1,9 @@
 'use strict';
 
 (function () {
-  var MIN_SCALE_VALUE = '25%';
-  var MAX_SCALE_VALUE = '100%';
-  var SCALE_STEP = '25%';
+  var MIN_SCALE_VALUE = 25;
+  var MAX_SCALE_VALUE = 100;
+  var SCALE_STEP = 25;
 
   window.imageScaling = {
     scaleValueElement: window.uploadImage.pictureEditingElement.querySelector('.scale__control--value')
@@ -13,18 +13,18 @@
   var scaleBiggerElement = window.uploadImage.pictureEditingElement.querySelector('.scale__control--bigger');
 
   var increaseScaleValue = function () {
-    var currentScaleValue = window.imageScaling.scaleValueElement.value;
+    var currentScaleValue = parseInt(window.imageScaling.scaleValueElement.value, 10);
     if (currentScaleValue !== MAX_SCALE_VALUE) {
-      var newScaleValue = parseInt(currentScaleValue, 10) + parseInt(SCALE_STEP, 10);
+      var newScaleValue = currentScaleValue +  SCALE_STEP;
       window.imageScaling.scaleValueElement.value = newScaleValue + '%';
     }
     return newScaleValue / window.utilities.MULTIPLICAND;
   };
 
   var decreaseScaleValue = function () {
-    var currentScaleValue = window.imageScaling.scaleValueElement.value;
+    var currentScaleValue = parseInt(window.imageScaling.scaleValueElement.value, 10);
     if (currentScaleValue !== MIN_SCALE_VALUE) {
-      var newScaleValue = parseInt(currentScaleValue, 10) - parseInt(SCALE_STEP, 10);
+      var newScaleValue = currentScaleValue -  SCALE_STEP;
       window.imageScaling.scaleValueElement.value = newScaleValue + '%';
     }
     return newScaleValue / window.utilities.MULTIPLICAND;
