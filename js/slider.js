2b838596a013c7d94d6c8b1b0b5e8dbef7f1e0fc
ilg2005 <igoldfarb10@gmail.com>
2018-12-15 23:47:26 +0300

    переменные - DOM-элементы слайдера (частично) вынесены за пределы window.slider

--- js/slider.js
+++ js/slider.js
@@ -2,17 +2,19 @@
 
 (function () {
   var SLIDER_PIN_WIDTH = 18;
-  //var sliderPinElement = window.imageUpload.pictureEditingElement.querySelector('.effect-level__pin');
+
+  var sliderPinElement = window.imageUpload.pictureEditingElement.querySelector('.effect-level__pin');
+  var sliderLineElement = window.imageUpload.pictureEditingElement.querySelector('.effect-level__depth');
+  var sliderEffectLevelValueElement = window.imageUpload.pictureEditingElement.querySelector('.effect-level__value');
 
   window.slider = {
     sliderPinElement: window.imageUpload.pictureEditingElement.querySelector('.effect-level__pin'),
     sliderLineElement: window.imageUpload.pictureEditingElement.querySelector('.effect-level__depth'),
-    sliderEffectLevelValueElement: window.imageUpload.pictureEditingElement.querySelector('.effect-level__value')
   };
 
   var convertPinPositionToEffectLevel = function () {
-    window.slider.sliderEffectLevelValueElement.value = parseInt(window.slider.sliderPinElement.style.left, 10);
-    var effectLevel = ((window.imageEffects.currentEffect.max - window.imageEffects.currentEffect.min) * window.slider.sliderEffectLevelValueElement.value / window.utilities.MULTIPLICAND) + window.imageEffects.currentEffect.min;
+    sliderEffectLevelValueElement.value = parseInt(sliderPinElement.style.left, 10);
+    var effectLevel = ((window.imageEffects.currentEffect.max - window.imageEffects.currentEffect.min) * sliderEffectLevelValueElement.value / window.utilities.MULTIPLICAND) + window.imageEffects.currentEffect.min;
     return effectLevel;
   };
 
@@ -22,8 +24,8 @@
 
   var sliderPinElementMouseDownHandler = function (evtMouseDown) {
     evtMouseDown.preventDefault();
-    var sliderLineWidthInPx = window.slider.sliderLineElement.offsetWidth;
-    var initialPinPositionInPx = window.slider.sliderPinElement.offsetLeft - SLIDER_PIN_WIDTH / 2;
+    var sliderLineWidthInPx = sliderLineElement.offsetWidth;
+    var initialPinPositionInPx = sliderPinElement.offsetLeft - SLIDER_PIN_WIDTH / 2;
     var startMouseX = evtMouseDown.clientX;
 
     var documentMouseMoveHandler = function (evtMouseMove) {
@@ -35,7 +37,7 @@
       var newPinPositionInPercent = Math.round(newPinPositionInPx * window.utilities.MULTIPLICAND / sliderLineWidthInPx);
 
       if (newPinPositionInPercent <= window.utilities.MULTIPLICAND && newPinPositionInPercent >= 0) {
-        window.slider.sliderPinElement.style.left = newPinPositionInPercent + '%';
+        sliderPinElement.style.left = newPinPositionInPercent + '%';
         changeEffectLevel(window.imageEffects.currentEffect.filterType, convertPinPositionToEffectLevel(), window.imageEffects.currentEffect.unit);
       }
     };
@@ -50,5 +52,5 @@
     document.addEventListener('mouseup', documentMouseUpHandler);
   };
 
-  window.slider.sliderPinElement.addEventListener('mousedown', sliderPinElementMouseDownHandler);
+  sliderPinElement.addEventListener('mousedown', sliderPinElementMouseDownHandler);
 })();
