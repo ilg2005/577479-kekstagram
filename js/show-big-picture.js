2877a86ffac00110a9c80e509ad6fc2879dfc104
ilg2005 <igoldfarb10@gmail.com>
2018-12-16 20:44:49 +0300

    добавлено скрытие bigPicturePreview по клику и Esc

--- js/show-big-picture.js
+++ js/show-big-picture.js
@@ -1,7 +1,10 @@
 'use strict';
 
 (function () {
+  var ESC_KEYCODE = 27;
   var bigPictureElement = document.querySelector('.big-picture');
+  var cancelPreviewElement = bigPictureElement.querySelector('#picture-cancel');
+  var inputCommentElement = bigPictureElement.querySelector('.social__footer-text');
 
   var generateCommentsFragment = function (comments) {
     var commentsFragment = document.createDocumentFragment();
@@ -12,7 +15,7 @@
 
       var newImg = document.createElement('img');
       newImg.classList.add('social__picture');
-      newImg.src = comment.avatar;
+      newImg.src = 'img/avatar-' + window.utilities.getRandomInRange(window.consts.MIN_AVATAR_NUM, window.consts.MAX_AVATAR_NUM) + '.svg';
       newImg.alt = 'Аватар комментатора фотографии';
       newImg.width = '35';
       newImg.height = '35';
@@ -20,7 +23,7 @@
 
       var newP = document.createElement('p');
       newP.classList.add('social__text');
-      newP.textContent = comment.message;
+      newP.textContent = comment;
       newLi.appendChild(newP);
 
       commentsFragment.appendChild(newLi);
@@ -41,14 +44,31 @@
     window.utilities.hideElement(bigPictureElement.querySelector('.comments-loader'));
   };
 
-  var documentClickHandler = function () {
+  var cancelPreview = function () {
+    window.utilities.hideElement(bigPictureElement);
+  };
 
+  var documentClickHandler = function (evt) {
+    window.utilities.showElement(bigPictureElement);
+    document.addEventListener('keydown', documentKeydownEscHandler);
+    if (evt.target === cancelPreviewElement) {
+      cancelPreview();
+    };
+  };
+
+  var documentKeydownEscHandler = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE && evt.target !== inputCommentElement) {
+      cancelPreview();
+      document.removeEventListener('keydown', documentKeydownEscHandler);
+    }
   };
 
   var init = function () {
     document.addEventListener('click', documentClickHandler);
-    generateBigPictureData(window.gallery[0]);
-    window.utilities.showElement(bigPictureElement);
+    console.log(window.gallery);
+    window.gallery.forEach(function (picture) {
+      generateBigPictureData(picture);
+    });
   };
 
   init();
