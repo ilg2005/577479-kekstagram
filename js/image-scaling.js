9d5d64c3fab86debfe0d594b9b648950a6f27cf2
ilg2005 <igoldfarb10@gmail.com>
2018-12-16 13:58:41 +0300

    исправлены ссылки в модуле scaling

--- js/image-scaling.js
+++ js/image-scaling.js
@@ -5,27 +5,29 @@
   var MAX_SCALE_VALUE = 100;
   var SCALE_STEP = 25;
 
+  var scaleValueElement = window.imageUpload.pictureEditingElement.querySelector('.scale__control--value');
+
   window.imageScaling = {
-    scaleValueElement: window.imageUpload.pictureEditingElement.querySelector('.scale__control--value')
+    scaleValueElement: scaleValueElement
   };
 
   var scaleSmallerElement = window.imageUpload.pictureEditingElement.querySelector('.scale__control--smaller');
   var scaleBiggerElement = window.imageUpload.pictureEditingElement.querySelector('.scale__control--bigger');
 
   var increaseScaleValue = function () {
-    var currentScaleValue = parseInt(window.imageScaling.scaleValueElement.value, 10);
+    var currentScaleValue = parseInt(scaleValueElement.value, 10);
     if (currentScaleValue !== MAX_SCALE_VALUE) {
       var newScaleValue = currentScaleValue + SCALE_STEP;
-      window.imageScaling.scaleValueElement.value = newScaleValue + '%';
+      scaleValueElement.value = newScaleValue + '%';
     }
     return newScaleValue / window.utilities.MULTIPLICAND;
   };
 
   var decreaseScaleValue = function () {
-    var currentScaleValue = parseInt(window.imageScaling.scaleValueElement.value, 10);
+    var currentScaleValue = parseInt(scaleValueElement.value, 10);
     if (currentScaleValue !== MIN_SCALE_VALUE) {
       var newScaleValue = currentScaleValue - SCALE_STEP;
-      window.imageScaling.scaleValueElement.value = newScaleValue + '%';
+      scaleValueElement.value = newScaleValue + '%';
     }
     return newScaleValue / window.utilities.MULTIPLICAND;
   };
