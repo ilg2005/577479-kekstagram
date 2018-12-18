373d5092494747797fc43c293c6736ab2b1f92b0
ilg2005 <igoldfarb10@gmail.com>
2018-12-18 04:51:24 +0300

    текущее состояние

--- js/hashtags-validation.js
+++ js/hashtags-validation.js
@@ -2,11 +2,23 @@
 
 (function () {
   var SPACE_KEYCODE = 32;
+  var MAX_HASHTAGS_NUMBER = 5;
   var hashtagsElement = document.querySelector('.text__hashtags');
+  var errorMessage;
+  var checkHashtagValidity = function () {
+    var hashtagsString = hashtagsElement.value;
+    var hashtags = hashtagsString.split(' ');
+    var hashtagsNumber = hashtags.length;
+    console.log(hashtagsNumber);
+    hashtagsElement.setAttribute('pattern', '#');
+    if (hashtagsElement.validity.patternMismatch) {
+      hashtagsElement.setCustomValidity('Нельзя указывать больше пяти хэш-тегов');
+    }
+  };
 
   var hastagsElementKeypressSpaceHandler = function (evt) {
     if (evt.keyCode === SPACE_KEYCODE) {
-      console.log('space');
+      checkHashtagValidity();
     }
   };
 
