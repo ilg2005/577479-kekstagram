6931faa938ba5e7db9cb4819a7b651b576a6c10a
ilg2005 <igoldfarb10@gmail.com>
2018-12-18 03:02:00 +0300

    добавлен обработчик на нажатие пробела

--- js/hashtags-validation.js
+++ js/hashtags-validation.js
@@ -1,5 +1,14 @@
 'us strict';
 
 (function () {
+  var SPACE_KEYCODE = 32;
+  var hashtagsElement = document.querySelector('.text__hashtags');
 
+  var hastagsElementKeypressSpaceHandler = function (evt) {
+    if (evt.keyCode === SPACE_KEYCODE) {
+      console.log('space');
+    }
+  };
+
+  hashtagsElement.addEventListener('keypress', hastagsElementKeypressSpaceHandler);
 })();
