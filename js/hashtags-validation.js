8c9a7480ad886ae86703e3c832ca7f2ea525a019
ilg2005 <igoldfarb10@gmail.com>
2018-12-18 14:16:01 +0300

    текущее состояние

--- js/hashtags-validation.js
+++ js/hashtags-validation.js
@@ -1,26 +1,79 @@
-'us strict';
+'use strict';
 
 (function () {
-  var SPACE_KEYCODE = 32;
   var MAX_HASHTAGS_NUMBER = 5;
+
   var hashtagsElement = document.querySelector('.text__hashtags');
-  var errorMessage;
-  var checkHashtagValidity = function () {
+
+  var getHashtagsArray = function () {
     var hashtagsString = hashtagsElement.value;
-    var hashtags = hashtagsString.split(' ');
-    var hashtagsNumber = hashtags.length;
-    console.log(hashtagsNumber);
-    hashtagsElement.setAttribute('pattern', '#');
-    if (hashtagsElement.validity.patternMismatch) {
-      hashtagsElement.setCustomValidity('Нельзя указывать больше пяти хэш-тегов');
+    hashtagsString = hashtagsString.trim();
+    var hashtagsArray = hashtagsString.split(' ');
+    return hashtagsArray;
+  };
+
+  var checkIfTooMuchHashtags = function (hashtagsArray) {
+    hashtagsElement.setCustomValidity('');
+    if (hashtagsArray.length > MAX_HASHTAGS_NUMBER) {
+      hashtagsElement.setCustomValidity('Нельзя указывать более пяти хэш-тегов');
     }
   };
 
-  var hastagsElementKeypressSpaceHandler = function (evt) {
-    if (evt.keyCode === SPACE_KEYCODE) {
-      checkHashtagValidity();
+  var checkDoubleOccurrence = function (hashtagsArray) {
+    hashtagsElement.setCustomValidity('');
+    var tempStorage = [];
+    hashtagsArray.forEach(function (hashtag) {
+      if (tempStorage.indexOf(hashtag) === -1) {
+        tempStorage.push(hashtag);
+      } else {
+        hashtagsElement.setCustomValidity('Один и тот же хэш-тег не может быть использован дважды');
+      }
+    });
+  };
+
+  var checkIfStartsWithHashSymbol = function (hashtag) {
+    hashtagsElement.setCustomValidity('');
+    if (hashtag.match(/(^#)/) === null) {
+      hashtagsElement.setCustomValidity('Хэш-тег должен начинаться с символа # (решётка)');
     }
   };
 
-  hashtagsElement.addEventListener('keypress', hastagsElementKeypressSpaceHandler);
+  var checkIfContainsOtherSymbols = function (hashtag) {
+    hashtagsElement.setCustomValidity('');
+    if (hashtag.match(/(^#.+)/) === null) {
+      hashtagsElement.setCustomValidity('Хэш-тег не может состоять только из одной решётки');
+    }
+  };
+
+  var checkIfContainsAnotherHashSymbol = function (hashtag) {
+    hashtagsElement.setCustomValidity('');
+    if (hashtag.match(/(^#[^#]+)/) === null) {
+      hashtagsElement.setCustomValidity('Хэш-теги должны разделяться пробелами');
+    }
+  };
+
+  var checkIfTooLong = function (hashtag) {
+    hashtagsElement.setCustomValidity('');
+    if (hashtag.match(/[^#]{1,19}/) === null) {
+      console.log(hashtag.match(/(^#)[^#]{1,19}/));
+      hashtagsElement.setCustomValidity('Максимальная длина одного хэш-тега должна быть 20 символов, включая решётку');
+    }
+  };
+
+  var hastagsElementBlurHandler = function () {
+    var hashtags = getHashtagsArray();
+
+    checkIfTooMuchHashtags(hashtags);
+    checkDoubleOccurrence(hashtags);
+
+    hashtags.forEach(function (hashtag) {
+      checkIfStartsWithHashSymbol(hashtag);
+      checkIfContainsOtherSymbols(hashtag);
+      checkIfContainsAnotherHashSymbol(hashtag);
+      checkIfTooLong(hashtag);
+    });
+
+  };
+
+  hashtagsElement.addEventListener('blur', hastagsElementBlurHandler);
 })();
