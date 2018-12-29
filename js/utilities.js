8ae8d23e7561ce9ae1bcf53f0a0d50504e91df76
ilg2005 <igoldfarb10@gmail.com>
2018-12-29 14:27:36 +0300

    из модуля utilities удалены не используемые больше функции

--- js/utilities.js
+++ js/utilities.js
@@ -4,22 +4,6 @@
   var MESSAGE_TIMEOUT = 1000;
   var ESC_KEYCODE = 27;
 
-  var createArrayFromRange = function (min, max) {
-    var numbers = [];
-    for (var i = min; i <= max; i++) {
-      numbers.push(i);
-    }
-    return numbers;
-  };
-
-  var getRandomIndex = function (array) {
-    return Math.round(Math.random() * (array.length - 1));
-  };
-
-  var getRandomValue = function (array) {
-    return array[getRandomIndex(array)];
-  };
-
   var removeServerMessage = function (element, timeout) {
     setTimeout(function () {
       element.remove();
@@ -36,10 +20,6 @@
     },
     hideElement: function (element) {
       element.classList.add('hidden');
-    },
-    getRandomInRange: function (min, max) {
-      var array = createArrayFromRange(min, max);
-      return getRandomValue(array);
     },
     renderErrorMessage: function (message) {
       var errorMessageElement = document.createElement('p');
