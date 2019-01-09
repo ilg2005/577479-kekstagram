0ba48bf43990c6c93edc80fd5ba020e64f8d9f72
ilg2005 <igoldfarb10@gmail.com>
2019-01-09 11:49:20 +0300

    текущее состояние

--- js/slider.js
+++ js/slider.js
@@ -39,10 +39,22 @@
   };
 
   var pinElementKeydownArrowHandler = function (evt) {
-    if (window.utilities.isArrowLeftEvent(evt)) {
-      console.log('left');
-    } else if (window.utilities.isArrowRightEvent(evt)) {
-      console.log('right');
+    if (parseInt(pinElement.style.left) <= window.utilities.MULTIPLICAND && parseInt(pinElement.style.left) >= 0) {
+      if (window.utilities.isArrowLeftEvent(evt)) {
+        console.log(pinElement.style.left);
+        console.log(parseInt(pinElement.style.left) - 1 + '%');
+        window.slider.currentPinPositionInPercent = parseInt(pinElement.style.left) - 1 + '%';
+        window.imageEffects.changeEffectLevel();
+        pinElement.style.left = window.slider.currentPinPositionInPercent;
+        console.log(pinElement.style.left);
+      } else if (window.utilities.isArrowRightEvent(evt)) {
+        console.log(pinElement.style.left);
+        console.log(parseInt(pinElement.style.left) + 1 + '%');
+        window.slider.currentPinPositionInPercent = parseInt(pinElement.style.left) + 1 + '%';
+        window.imageEffects.changeEffectLevel();
+        pinElement.style.left = window.slider.currentPinPositionInPercent;
+        console.log(pinElement.style.left);
+      }
     }
   };
 
