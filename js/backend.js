9b06ff6708a82b9c85101f7bcca7099f75b21d0d
ilg2005 <igoldfarb10@gmail.com>
2019-01-12 15:50:54 +0300

    задание на доступность выполнено

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/kekstagra';
+  var URL = 'https://js.dump.academy/kekstagram';
   var SERVER_RESPONSE_OK = 200;
   var LOADING_TIMEOUT = 10000;
 
